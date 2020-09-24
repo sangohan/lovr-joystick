37fd480907821c0c368d086e33ae5b675fab390d
Rabia Alhaffar <steria773@gmail.com>
2020-09-25 01:41:57 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,17 +1,15 @@
 lovr.joystick = require("lovr-joystick")
-
 local draw = false
 
 function lovr.draw()
-  lovr.graphics.clear()
   if draw then
     lovr.graphics.print(lovr.joystick.getName(0), 0, 0, -5, 1)
   end
 end
 
 function lovr.update()
-  if lovr.joystick.isDown(0, "x") then
-    if lovr.joystick.isAvailable(0) then
+  if lovr.joystick.isAvailable(0) then
+    if lovr.joystick.isDown(0, "x") then
       draw = true
     else
       draw = false
