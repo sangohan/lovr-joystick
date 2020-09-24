f5c5f69a3fc27dbfd2820ea22f03377f49e5cf5b
Rabia Alhaffar <steria773@gmail.com>
2020-09-25 01:40:00 +0300

    Improved example!

--- main.lua
+++ main.lua
@@ -5,16 +5,16 @@ local draw = false
 function lovr.draw()
   lovr.graphics.clear()
   if draw then
-    if lovr.joystick.isAvailable(0) then
-      lovr.graphics.print(lovr.joystick.getName(0), 0, 0, -5, 1)
-    end
+    lovr.graphics.print(lovr.joystick.getName(0), 0, 0, -5, 1)
   end
 end
 
 function lovr.update()
   if lovr.joystick.isDown(0, "x") then
-    draw = true
-  else
-    draw = false
+    if lovr.joystick.isAvailable(0) then
+      draw = true
+    else
+      draw = false
+    end
   end
 end
